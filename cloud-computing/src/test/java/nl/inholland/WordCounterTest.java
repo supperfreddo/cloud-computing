fee1c0c6a7ff787974362256d67b358fd2a72087
fredsoliede <fredwillemsen1999@gmail.com>
2023-10-10 23:54:25 +0200

    tests
    - added more tests
    - added gotenberg-testdata

--- cloud-computing/src/test/java/nl/inholland/WordCounterTest.java
+++ cloud-computing/src/test/java/nl/inholland/WordCounterTest.java
@@ -1,9 +1,37 @@
 package nl.inholland;
 
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WordCounterTest {
+
+    // Test that the word counter returns more than 0 words
+    @Test
+    public void wordCounterMoreThanZero() {
+        // Init word counter
+        WordCounter wordCounter = new WordCounter();
+
+        // Get word counts
+        int size = wordCounter.getWordCounts().size();
+
+        // Assert that there are more than 0 words
+        assertEquals(true, size > 0);
+    }
+
+    // Test that the multi-threaded word counter returns more than 0 words
+    @Test
+    public void wordCounterMultiThreadedMoreThanZero() {
+        // Init word counter
+        WordCounterMultiThreaded wordCounterMultiThreaded = new WordCounterMultiThreaded();
+
+        // Get word counts
+        int size = wordCounterMultiThreaded.getWordCounts().size();
+
+        // Assert that there are more than 0 words
+        assertEquals(true, size > 0);
+    }
 
     // Test that both implementations return the same number of words
     // Run the test 10 times beacuse of multi-threading
